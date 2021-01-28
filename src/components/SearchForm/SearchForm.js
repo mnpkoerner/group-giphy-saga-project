383c841485c41fbaa63cad766a9b956889daf102
Woody Kromar <wkromar12@gmail.com>
2021-01-28 14:28:05 -0600

    many components partially made

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,29 @@
+import React, {useState}from 'react';
+import { useDispatch } from 'react-redux';
+
+
+const SearchForm = () =>{
+const dispatch = useDispatch();
+let [searchItem, setSearchItem] = useState('')
+
+const newSearch = (event) => {
+    event.preventDefault();
+    dispatch({type: "SEARCH_GIPHY", payload: searchItem})
+}
+
+
+        return(
+            <div>
+                <h2>Search using GIF tags (funny etc...)</h2>
+                <form onSubmit={newSearch}>
+                <input type='text' 
+                value={searchItem} 
+                onChange={(event)=> setSearchItem(event.target.value)} 
+                />
+                <button type='Submit'>Search</button>
+                </form>
+            </div>
+        )
+}
+
+export default SearchForm;

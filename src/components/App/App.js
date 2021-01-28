fdecf5a4b9e1bab59bd5bef8fbb33f56eadfda07
mnpkoerner <mnpkoerner@gmail.com>
2021-01-28 13:07:30 -0600

    remove tests from App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import Header from '../Header/Header'
-import{useEffect} from 'react'
-import {useDispatch} from 'react-redux'
-
-
-
 
 function App(props) {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    console.log('page loaded');
-    // dispatch an action to page
-    dispatch({type: 'TEST'})
-  }, []);
-
   return (
     <div>
       <Header />

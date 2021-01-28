d2dcc1b8715f397127ea835ad8cd0a9852201f59
mnpkoerner <mnpkoerner@gmail.com>
2021-01-28 11:01:50 -0600

    error on DOM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Header from '../Header/Header'
+import{useEffect} from 'react'
+import {useDispatch} from 'react-redux'
+
+
+
 
 function App(props) {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    console.log('page loaded');
+    // dispatch an action to page
+    dispatch({type: 'TEST'})
+  }, []);
+
   return (
     <div>
       <Header />
--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//saga
+import createSagaMiddleware from 'redux-saga';
+import {takeEvery, put} from 'redux-saga/effects';
+import axios from 'axios';
+import logger from 'redux-logger';
+
+//watcherSaga holds all sagas
+function* watcherSaga() {
+    yield takeEvery('TEST', testRoute);
+}
+
+function* testRoute(){
+    console.log('in test route')
+    yield put({ type: 'TEST_REDUCE', payload: 'test complete'})
+}
+
+
+//reducers
+const testReducer = (state = '', action) => {
+    if(action.type === 'TEST_REDUCE'){
+        return state = action.payload
+    }
+    return state;
+}
+
+
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+    combineReducers({
+        testReducer
+    }), applyMiddleware(sagaMiddleware, logger)
+);
+
+sagaMiddleware.run(watcherSaga)
+
+ReactDOM.render(<App />);

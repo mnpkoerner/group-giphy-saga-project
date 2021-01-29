82859d076152ddf42bcdd18d88a62284fe95a8ca
Woody Kromar <wkromar12@gmail.com>
2021-01-28 18:16:06 -0600

    pushing staged commits

--- src/components/FavoritesForm/FavoritesForm.js
+++ src/components/FavoritesForm/FavoritesForm.js
@@ -3,39 +3,33 @@
 const FavoritesForm= () =>{
 const dispatch = useDispatch();
 let [searchCategory, setSearchCategory] = useState('');
-let [favorite, setFavorite] = useState('')
+
     
 
 const newCategory = (event) => {
     event.preventDefault();
     dispatch({type: 'GET_FAVORITE', payload: favoriteItem})
 }
-const setAsFavorite = (event) =>{
- setFavorite
-}
 
 
 
-return(
+
+
+     return(
     <div>
-        <button onClick={setAsFavorite}>Favorite (heart)</button>
-    </div>
-)
+        <form onSubmit={newSearch}>
+            <input type="text" value = {searchCategory} list="chooseGiphy"/> 
+                 <datalist id="chooseGiphy">
+                 <option value="funny"/>
+                 <option value="cohort"/>
+                 <option value="cartoon"/>
+                 <option value="nsfw"/>
+                 <option value="meme"/>
+                 </datalist>
+                 <button type='Submit'></button>
 
-//     return(
-//     <div>
-//         <form onSubmit={newSearch}>
-//             <input type="text" value = {searchCategory} list="chooseGiphy"/>
-//                 <datalist id="chooseGiphy">
-//                 <option value="funny"/>
-//                 <option value="cohort"/>
-//                 <option value="cartoon"/>
-//                 <option value="nsfw"/>
-//                 <option value="meme"/>
-//                 </datalist>
-//                 <button type='Submit'></button>
-//         </form>
-//     </div>
-// )
+         </form>
+     </div>
+ )
 }
 //map through favorites in database and post a selection
--- src/components/SearchList/SearchList.js
+++ src/components/SearchList/SearchList.js
@@ -1,19 +1,37 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector, useState} from 'react-redux';
 
 const SearchList = () =>{
 
 const dispatch = useDispatch();
-const searchReducer = useSelector(store => store.SOMETHINGREDUCER)
+const searchReducer = useSelector(store => store.searchReducer);
+let [favoriteItem, setFavoriteItem] = useState(
+    {
+        url:'',
+        title: ''
+    }
+);
 
+// useEffect(() => {
+//         console.log('component did mount');
+//         // dispatch an action to request the search items from api
+//         dispatch({type:'NULL'})
+//     }, []); 
 
-useEffect(() => {
-        console.log('component did mount');
-        // dispatch an action to request the search items from api
-        dispatch({type:'NULL'})
-    }, []); 
-
-
+    favoriteSwitch = (url, title) =>{
+        currentValue = document.getElementById ('switch').value;
+        if(currentValue == "Off") { 
+             document.getElementById('switch').value="On" 
+            setFavoriteItem({
+                url:url,
+                title:title
+                })
+             console.log("item sent to favorites", favoriteItem);  
+             dispatch({ type:'SEARCH_GIPHY', payload: favoriteItem})
+        }else{
+        document.getElementById('switch').value="Off";
+        }
+    }
         return(
             <div>
                 <h2>Search Results</h2>
@@ -21,8 +39,9 @@ useEffect(() => {
                      return(
                         <ul>
                             <li>
-                            <p></p>
+                                <p>{searchItem.data?.title}</p>
                                 <img src={searchItem.data?.image_url} width="400" height="300"></img>
+                                <button  id ="switch" value="Off" onClick={() => favoriteSwitch(searchItem.data?.title, searchItem.data?.image_url)}>Favorite</button>
                             </li>
                         </ul>
                      )
